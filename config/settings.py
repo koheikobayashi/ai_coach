58e8df8e65712cefebf9a04a21f6532e47fe9206
kkobayashiHMC <kohei.kobayashi@hmcom.co.jp>
2026-02-01 17:44:22 +0900

    renderデプロイ対応

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
